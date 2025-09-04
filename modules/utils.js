7597498414cb08b4e7cb6a973eeba82febb964d6
Tooster <5300963+T3sT3ro@users.noreply.github.com>
2025-09-04 07:46:28 +0200

    WIP basins debug/improvement

---
+++ modules/utils.js
@@ -0,0 +1,22 @@
+// Generate letter sequence: a, b, c, ..., z, aa, ab, ac, ...
+/**
+ * Generate a letter sequence for basin naming (A, B, C, ..., Z, AA, AB, etc.)
+ * @param {number} index - The index to convert to letters
+ * @returns {string} Letter sequence string
+ */
+function generateLetterSequence(index) {
+  let result = "";
+  let num = index;
+
+  do {
+    result = String.fromCharCode(65 + (num % 26)) + result;
+    num = Math.floor(num / 26) - 1;
+  } while (num >= 0);
+
+  return result;
+}
+
+/**
+ * @type {{generateLetterSequence: (index: number) => string}}
+ */
+exports.generateLetterSequence = generateLetterSequence;
